afe95bc8d68df7d2e4eee7351d3c3e0ca230cc7c
fiahfy <d.fiahfy@gmail.com>
2019-02-07 23:01:15 +0900

    Update twitter.js

--- functions/twitter.js
+++ functions/twitter.js
@@ -3,9 +3,29 @@ const fs = require('fs')
 
 require('dotenv').config()
 
+const Group = {
+    animare: 'animare',
+    honeyStrap: 'honey_strap'
+}
+
+const UserId = {
+    [Group.animare]: '',
+    [Group.honeyStrap]: 1007247110167674900, // @HNST_official
+}
+
+
+const admin = require('firebase-admin')
+admin.initializeApp()
+
 ;(async () => {
     try {
-        console.log(process.env)
+        const querySnapshot = await admin.firestore().collection('anihani-schedule-tweets')
+            .where('user_id', '==', UserId[Group.honeyStrap])
+            .orderBy("created_at")
+            .limit(1);
+        console.log(querySnapshot)
+        
+            return
 
         const client = new Twitter({
             consumer_key: process.env.TWITTER_CONSUMER_KEY,
