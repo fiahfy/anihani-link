683656f3320493ea0760ebc97bd4b99cfa8dae4b
fiahfy <d.fiahfy@gmail.com>
2019-02-28 20:41:04 +0900

    Update MemberListTile.vue

--- src/components/MemberListTile.vue
+++ src/components/MemberListTile.vue
@@ -1,14 +1,17 @@
 <template>
   <v-list-tile :to="'/member?id=' + member.id">
     <v-list-tile-avatar size="96" color="grey darken-4">
-      <app-image :src="`/img/members/${member.id}_96x96.pnga`" />
+      <app-image
+        :src="`/img/members/${member.id}_96x96.png`"
+        :lazy-src="`/img/members/${member.id}_48x48.png`"
+      />
     </v-list-tile-avatar>
 
     <v-list-tile-content>
       <v-list-tile-title v-text="member.name_ja" />
       <v-list-tile-sub-title>
-        <p class="my-2">{{ member.group.name_ja }}</p>
-        <p>{{ member.description }}</p>
+        <p class="my-1">{{ member.group.name_ja }}</p>
+        <p class="mb-0">{{ member.description }}</p>
       </v-list-tile-sub-title>
     </v-list-tile-content>
   </v-list-tile>
@@ -33,9 +36,10 @@ export default {
 <style scoped>
 div >>> .v-list__tile {
   height: 132px;
+  padding-left: 32px;
 }
 .v-list__tile__avatar {
-  min-width: 112px;
+  min-width: 128px;
 }
 .v-list__tile__sub-title > p:last-child {
   white-space: initial;
